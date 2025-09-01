88e099d0e7889137f6faba5ae3d228f04082d255
danalmeidap <dbatistadealmeida23@gmail.com>
2025-09-01 01:54:27 -0300

    feat:Docker-compose and core configs

--- tdd_project/app.py
+++ tdd_project/app.py
@@ -1,8 +1,35 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+from tdd_project.core.config import settings
+
+
+class App(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            *args,
+            **kwargs,
+            version='0.1.0',
+            title=settings.PROJECT_NAME,
+            root_path=settings.ROOT_PATH,
+        )
+
+
+app = App()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.get(settings.API_V1_STR + '/healthcheck')
+def healthcheck():
+    return {'status': 'ok'}
+
+
+@app.get(settings.API_V1_STR + '/info')
+def info():
+    return {
+        'project_name': settings.PROJECT_NAME,
+        'database_url': settings.DATABASE_URL,
+    }
